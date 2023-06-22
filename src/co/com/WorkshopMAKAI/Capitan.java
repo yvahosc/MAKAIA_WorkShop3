133fbd9ee8ac4ea6db25ca3292c13d1f723f4ae1
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-22 17:40:37 -0500

    Creación de la clase Capitan con sus atributos y constructor.

---
+++ src/co/com/WorkshopMAKAI/Capitan.java
@@ -0,0 +1,13 @@
+package co.com.WorkshopMAKAI;
+
+public class Capitan {
+    private String nombre;
+    private String apellido;
+    private String matriculaDeNavegacion;
+
+    public Capitan(String nombre, String apellido, String matriculaDeNavegacion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matriculaDeNavegacion = matriculaDeNavegacion;
+    }
+}
