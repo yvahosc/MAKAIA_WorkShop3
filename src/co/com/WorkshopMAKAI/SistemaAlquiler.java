e52340ad35b86ea696be87b49b1ada3519baf73d
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-28 18:13:27 -0500

    Modificación de la estructura de los métodos propios de la clase para implementar validaciones.

--- src/co/com/WorkshopMAKAI/SistemaAlquiler.java
+++ src/co/com/WorkshopMAKAI/SistemaAlquiler.java
@@ -5,63 +5,112 @@ public class SistemaAlquiler {
     public Embarcacion[] embarcaciones = new Embarcacion[10];
     private int cantidadDeEmbarcaciones = 0;
 
-    // [  null  |  yate2   |   velero1   |   null  |   null  | null   |
-    // null   |   null  |   null  |  null   ] - cantidadDeEmbarcaciones = 2
-    // agregarEmbarcacion(Yate3)
-    // [  null  |  yate2   |   Yate3   |   null  |   null  | null   |
-    // null   |   null  |   null  |  null   ] - cantidadDeEmbarcaciones = 3
     public void agregarEmbarcacion(Embarcacion embarcacion){
+
         if (cantidadDeEmbarcaciones < 10){
-            embarcaciones[cantidadDeEmbarcaciones] = embarcacion;
-            cantidadDeEmbarcaciones++;
+            if(this.estaEmbarcacionEnElSistema(embarcacion) == false){
+                for (int i = 0; i < embarcaciones.length; i++) {
+                    if (embarcaciones[i] == null){
+                        embarcaciones[i] = embarcacion;
+                        cantidadDeEmbarcaciones++;
+                        embarcacion.setDisponible(true);
+                        System.out.println("La embarcación: " + embarcacion.toString()
+                                + ", ha sido agregada exitosamente al sistema.");
+                        break;
+                    }
+                }
+            }else{
+                System.out.println("Error. La embarcación: " + embarcacion.toString()
+                        + ", ya existe en el sistema.");
+            }
         } else{
-            System.out.println("No es posible agregara más embarcaciones.");
+            System.out.println("Error. No es posible agregar más " +
+                    "embarcaciones.");
         }
-        embarcacion.setDisponible(true);
     }
 
     public void alquilarEmbarcacion(Embarcacion embarcacion){
-        if (embarcacion.isDisponible() == true){
-            embarcacion.setDisponible(false);
-            System.out.println("Alquiler exitoso. Embarcación: " + embarcacion.toString());
-            embarcacion.calcularMontoAlquiler();
-        } else{
-            System.out.println("La embarcación no está disponible.");
+        if(this.estaEmbarcacionEnElSistema(embarcacion) == true){
+            if (embarcacion.isDisponible() == true){
+                embarcacion.setDisponible(false);
+                System.out.println("Alquiler exitoso. Embarcación: " + embarcacion.toString());
+                embarcacion.calcularMontoAlquiler();
+            } else{
+                System.out.println("Error. La embarcación" + embarcacion.toString()
+                        + ", no está disponible.");
+            }
+        }else{
+            this.mensajeEmbarcacionNoEstaEnelSistema(embarcacion);
         }
     }
 
     public void devolverEmbarcacion(Embarcacion embarcacion){
-        if (embarcacion.isDisponible() == false){
-            embarcacion.setDisponible(true);
-            System.out.println("La embarcación: " + embarcacion.toString() +
-                    " ingresó nuevamente al sistema y está disponible para " +
-                    "ser alquilada nuevamente.");
-        } else{
-            System.out.println("Error la embarcación no ha sido prestada.");
+        if(this.estaEmbarcacionEnElSistema(embarcacion) == true){
+            if (embarcacion.isDisponible() == false){
+                embarcacion.setDisponible(true);
+                System.out.println("La embarcación: " + embarcacion.toString() +
+                        ", ingresó nuevamente al sistema y está disponible " +
+                        "para ser alquilada nuevamente.");
+            } else{
+                System.out.println("Error la embarcación "+ embarcacion.toString()
+                        + ", no ha sido alquilada.");
+            }
+        }else{
+            this.mensajeEmbarcacionNoEstaEnelSistema(embarcacion);
         }
     }
-    // [  yate1  |  yate2   |   velero1   |   null  |   null  | null   |
-    // null   |   null  |   null  |  null   ] - cantidadDeEmbarcaciones = 3
-    // embarcaciones[0] = null;
-    // [  null  |  yate2   |   velero1   |   null  |   null  | null   |
-    // null   |   null  |   null  |  null   ] - cantidadDeEmbarcaciones = 2
-    public void venderEmbarcacion(Embarcacion embarcacion){
-        // if(embarcacion.getClass() == Yate.class){
-        //
-        // }else{
-        //
-        //}
 
-        if (embarcacion.isDisponible() == true){
-            if(embarcacion.isVendible() == true){
-                embarcacion.setDisponible(false);
+    public void venderEmbarcacion(Embarcacion embarcacion){
+
+        if(this.estaEmbarcacionEnElSistema(embarcacion)){
+            if(embarcacion.isDisponible() && embarcacion.isVendible())
+            {
+                for (int i = 0; i < embarcaciones.length; i++) {
+                    if (embarcaciones[i] == embarcacion){
+                        embarcaciones[i] = null;
+                        cantidadDeEmbarcaciones--;
+                        embarcacion.setDisponible(false);
+                        break;
+                    }
+                }
                 System.out.println("La embarcación: " + embarcacion.toString() +
-                        " fue vendida exitosamente.");
+                        ", fue vendida exitosamente.");
             }else{
-                System.out.println("Error. La embarcación no puede ser vendida.");
+                System.out.println("Error. La embarcación: " + embarcacion.toString()
+                        + ", no puede ser vendida.");
             }
-        } else{
-            System.out.println("La embarcación no está disponible.");
+        }else{
+            this.mensajeEmbarcacionNoEstaEnelSistema(embarcacion);
         }
+    }
+
+    public void mostrarEmbarcacionesEnElSistema(){
+        boolean embarcacionesVacio = true;
+
+        System.out.println("Embarcaciones:");
+        for (int i = 0; i < embarcaciones.length; i++) {
+            if(embarcaciones[i] != null){
+                System.out.println(embarcaciones[i].toString());
+                embarcacionesVacio = false;
+            }
+        }
+        if(embarcacionesVacio){
+            System.out.println("No hay embarcaciones en el sistema.");
+        }
+    }
+
+    public boolean estaEmbarcacionEnElSistema(Embarcacion embarcacion){
+        boolean estaEmbarcacion = false;
+        for (int i = 0; i < embarcaciones.length; i++) {
+            if (embarcaciones[i] == embarcacion){
+                estaEmbarcacion = true;
+            }
+        }
+        return estaEmbarcacion;
+    }
+
+    public void mensajeEmbarcacionNoEstaEnelSistema(Embarcacion embarcacion){
+        System.out.println("Error. La embarcación: " + embarcacion.toString()
+                + ", no existe en el sistema.");
     }
 }
